8780df1552af34beb8d3bdd1edbd1d6162e2ca8e
Reza <mr.naeemabadi@gmail.com>
2021-02-09 17:30:56 +0100

    Adding support for device location and username in the Flash

--- ESP32/lib/Storage/StoreHub.cpp
+++ ESP32/lib/Storage/StoreHub.cpp
@@ -4,6 +4,8 @@ std::string Storage_WiFi_SSID;
 std::string Storage_WiFi_Pass;
 std::string Storage_Home_Name;
 std::string Storage_Azure_IoT;
+std::string Storage_Dev_Locat;
+std::string Storage_User_Name;
 
 void EraseFlash(bool safe)
 {
@@ -13,6 +15,8 @@ void EraseFlash(bool safe)
         erase_len += EEPROM.read(EEPROM_WIFI_PASSWORD_LEN_ADDR);
         erase_len += EEPROM.read(EEPROM_AZURE_IOT_HUB_LEN_ADDR);
         erase_len += EEPROM.read(EEPROM_GOOGLE_HOME_NAME_LEN_ADDR);
+        erase_len += EEPROM.read(EEPROM_DEVICE_LOCATION_LEN_ADDR);
+        erase_len += EEPROM.read(EEPROM_USER_NAME_LEN_ADDR);
         for (size_t i = 0; i < erase_len; i++)
             EEPROM.writeChar(EEPROM_STORAGE_START_ADDR + i, 0);
     }
@@ -21,94 +25,138 @@ void EraseFlash(bool safe)
     EEPROM.write(EEPROM_WIFI_PASSWORD_LEN_ADDR, 0);
     EEPROM.write(EEPROM_GOOGLE_HOME_NAME_LEN_ADDR, 0);
     EEPROM.write(EEPROM_AZURE_IOT_HUB_LEN_ADDR, 0);
+    EEPROM.write(EEPROM_DEVICE_LOCATION_LEN_ADDR, 0);
+    EEPROM.write(EEPROM_USER_NAME_LEN_ADDR, 0);
 
     EEPROM.commit();
 }
 
 void ReadFlash()
 {
+    uint8_t pos = 0;
+    uint8_t len = 0;
     Storage_WiFi_SSID.clear();
-    uint8_t ssid_len = EEPROM.read(EEPROM_WIFI_SSID_NAME_LEN_ADDR);
-    if (ssid_len == EEPROM_UNINITIALIZED_VALUE)
-        ssid_len = 0;
-    for (size_t i = 0; i < ssid_len; i++)
-        Storage_WiFi_SSID.push_back(EEPROM.readChar(i + EEPROM_STORAGE_START_ADDR));
+    len = EEPROM.read(EEPROM_WIFI_SSID_NAME_LEN_ADDR);
+    if (len == EEPROM_UNINITIALIZED_VALUE)
+        len = 0;
+    for (size_t i = 0; i < len; i++)
+        Storage_WiFi_SSID.push_back(EEPROM.readChar(i + pos + EEPROM_STORAGE_START_ADDR));
+    pos += len;
 
     Storage_WiFi_Pass.clear();
-    uint8_t pass_len = EEPROM.read(EEPROM_WIFI_PASSWORD_LEN_ADDR);
-    if (pass_len == EEPROM_UNINITIALIZED_VALUE)
-        pass_len = 0;
-    for (size_t i = 0; i < pass_len; i++)
-        Storage_WiFi_Pass.push_back(EEPROM.readChar(i + ssid_len + EEPROM_STORAGE_START_ADDR));
+    len = EEPROM.read(EEPROM_WIFI_PASSWORD_LEN_ADDR);
+    if (len == EEPROM_UNINITIALIZED_VALUE)
+        len = 0;
+    for (size_t i = 0; i < len; i++)
+        Storage_WiFi_Pass.push_back(EEPROM.readChar(i + pos + EEPROM_STORAGE_START_ADDR));
+    pos += len;
 
     Storage_Home_Name.clear();
-    uint8_t home_len = EEPROM.read(EEPROM_GOOGLE_HOME_NAME_LEN_ADDR);
-    if (home_len == EEPROM_UNINITIALIZED_VALUE)
-        home_len = 0;
-    for (size_t i = 0; i < home_len; i++)
-        Storage_Home_Name.push_back(EEPROM.readChar(i + ssid_len + pass_len + EEPROM_STORAGE_START_ADDR));
+    len = EEPROM.read(EEPROM_GOOGLE_HOME_NAME_LEN_ADDR);
+    if (len == EEPROM_UNINITIALIZED_VALUE)
+        len = 0;
+    for (size_t i = 0; i < len; i++)
+        Storage_Home_Name.push_back(EEPROM.readChar(i + pos + EEPROM_STORAGE_START_ADDR));
+    pos += len;
 
     Storage_Azure_IoT.clear();
-    uint8_t azure_len = EEPROM.read(EEPROM_AZURE_IOT_HUB_LEN_ADDR);
-    if (azure_len == EEPROM_UNINITIALIZED_VALUE)
-        azure_len = 0;
-    for (size_t i = 0; i < azure_len; i++)
-        Storage_Azure_IoT.push_back(EEPROM.readChar(i + ssid_len + pass_len + home_len + EEPROM_STORAGE_START_ADDR));
+    len = EEPROM.read(EEPROM_AZURE_IOT_HUB_LEN_ADDR);
+    if (len == EEPROM_UNINITIALIZED_VALUE)
+        len = 0;
+    for (size_t i = 0; i < len; i++)
+        Storage_Azure_IoT.push_back(EEPROM.readChar(i + pos + EEPROM_STORAGE_START_ADDR));
+    pos += len;
+
+    Storage_Dev_Locat.clear();
+    len = EEPROM.read(EEPROM_DEVICE_LOCATION_LEN_ADDR);
+    if (len == EEPROM_UNINITIALIZED_VALUE)
+        len = 0;
+    for (size_t i = 0; i < len; i++)
+        Storage_Dev_Locat.push_back(EEPROM.readChar(i + pos + EEPROM_STORAGE_START_ADDR));
+    pos += len;
+
+    Storage_User_Name.clear();
+    len = EEPROM.read(EEPROM_USER_NAME_LEN_ADDR);
+    if (len == EEPROM_UNINITIALIZED_VALUE)
+        len = 0;
+    for (size_t i = 0; i < len; i++)
+        Storage_User_Name.push_back(EEPROM.readChar(i + pos + EEPROM_STORAGE_START_ADDR));
+    pos += len;
 }
 
-void WriteFlash(std::string wifi_ssid, std::string wifi_pass, std::string home_name, std::string iot_hub_connection_string)
+void WriteFlash(std::string wifi_ssid, std::string wifi_pass, std::string home_name, std::string iot_hub_connection_string, std::string device_location, std::string username)
 {
-    Serial.println("Writing to the flash. SSID: " + String(wifi_ssid.c_str()) + " Password: " + String(wifi_pass.c_str()) + " Home Name: " + String(home_name.c_str()));
     Storage_WiFi_SSID = wifi_ssid;
     Storage_WiFi_Pass = wifi_pass;
     Storage_Home_Name = home_name;
     Storage_Azure_IoT = iot_hub_connection_string;
+    Storage_Dev_Locat = device_location;
+    Storage_User_Name = username;
 
-    uint8_t ssid_len = 0;
-    uint8_t pass_len = 0;
-    uint8_t home_len = 0;
-    uint8_t azure_len = 0;
+    uint8_t len = 0;
+    uint8_t pos = 0;
 
-    if (!wifi_ssid.empty())
-        ssid_len = wifi_ssid.length();
-    EEPROM.write(EEPROM_WIFI_SSID_NAME_LEN_ADDR, ssid_len);
-    for (size_t i = 0; i < ssid_len; i++)
-        EEPROM.writeChar(EEPROM_STORAGE_START_ADDR + i, wifi_ssid.at(i));
+    len = !wifi_ssid.empty() ? wifi_ssid.length() : 0;
+    EEPROM.write(EEPROM_WIFI_SSID_NAME_LEN_ADDR, len);
+    for (size_t i = 0; i < len; i++)
+        EEPROM.writeChar(EEPROM_STORAGE_START_ADDR + pos + i, wifi_ssid.at(i));
+    pos += len;
 
-    if (!wifi_pass.empty())
-        pass_len = wifi_pass.length();
-    EEPROM.write(EEPROM_WIFI_PASSWORD_LEN_ADDR, pass_len);
-    for (size_t i = 0; i < pass_len; i++)
-        EEPROM.writeChar(EEPROM_STORAGE_START_ADDR + ssid_len + i, wifi_pass.at(i));
+    len = !wifi_pass.empty() ? wifi_pass.length() : 0;
+    EEPROM.write(EEPROM_WIFI_PASSWORD_LEN_ADDR, len);
+    for (size_t i = 0; i < len; i++)
+        EEPROM.writeChar(EEPROM_STORAGE_START_ADDR + pos + i, wifi_pass.at(i));
+    pos += len;
 
-    if (!home_name.empty())
-        home_len = home_name.length();
-    EEPROM.write(EEPROM_GOOGLE_HOME_NAME_LEN_ADDR, home_len);
-    for (size_t i = 0; i < home_len; i++)
-        EEPROM.writeChar(EEPROM_STORAGE_START_ADDR + ssid_len + pass_len + i, home_name.at(i));
+    len = !home_name.empty() ? home_name.length() : 0;
+    EEPROM.write(EEPROM_GOOGLE_HOME_NAME_LEN_ADDR, len);
+    for (size_t i = 0; i < len; i++)
+        EEPROM.writeChar(EEPROM_STORAGE_START_ADDR + pos + i, home_name.at(i));
+    pos += len;
 
-    if (!iot_hub_connection_string.empty())
-        azure_len = iot_hub_connection_string.length();
-    EEPROM.write(EEPROM_AZURE_IOT_HUB_LEN_ADDR, azure_len);
-    for (size_t i = 0; i < azure_len; i++)
-        EEPROM.writeChar(EEPROM_STORAGE_START_ADDR + ssid_len + pass_len + home_len + i, iot_hub_connection_string.at(i));
+    len = !iot_hub_connection_string.empty() ? iot_hub_connection_string.length() : 0;
+    EEPROM.write(EEPROM_AZURE_IOT_HUB_LEN_ADDR, len);
+    for (size_t i = 0; i < len; i++)
+        EEPROM.writeChar(EEPROM_STORAGE_START_ADDR + pos + i, iot_hub_connection_string.at(i));
+    pos += len;
+
+    len = !device_location.empty() ? device_location.length() : 0;
+    EEPROM.write(EEPROM_DEVICE_LOCATION_LEN_ADDR, len);
+    for (size_t i = 0; i < len; i++)
+        EEPROM.writeChar(EEPROM_STORAGE_START_ADDR + pos + i, device_location.at(i));
+    pos += len;
+
+    len = !username.empty() ? username.length() : 0;
+    EEPROM.write(EEPROM_USER_NAME_LEN_ADDR, len);
+    for (size_t i = 0; i < len; i++)
+        EEPROM.writeChar(EEPROM_STORAGE_START_ADDR + pos + i, username.at(i));
+    pos += len;
 
     EEPROM.commit();
 }
 
 void WriteAzureIoTHub(std::string connection_string)
 {
-    WriteFlash(Storage_WiFi_SSID, Storage_WiFi_Pass, Storage_Home_Name, connection_string);
+    WriteFlash(Storage_WiFi_SSID, Storage_WiFi_Pass, Storage_Home_Name, connection_string, Storage_Dev_Locat, Storage_User_Name);
 }
 
 void WriteFlashWiFi(std::string wifi_ssid, std::string wifi_pass)
 {
-    WriteFlash(wifi_ssid, wifi_pass, Storage_Home_Name, Storage_Azure_IoT);
+    WriteFlash(wifi_ssid, wifi_pass, Storage_Home_Name, Storage_Azure_IoT, Storage_Dev_Locat, Storage_User_Name);
 }
 
 void WriteFlashHomeName(std::string home_name)
 {
-    WriteFlash(Storage_WiFi_SSID, Storage_WiFi_Pass, home_name, Storage_Azure_IoT);
+    WriteFlash(Storage_WiFi_SSID, Storage_WiFi_Pass, home_name, Storage_Azure_IoT, Storage_Dev_Locat, Storage_User_Name);
+}
+
+void WriteUsername(std::string username)
+{
+    WriteFlash(Storage_WiFi_SSID, Storage_WiFi_Pass, Storage_Home_Name, Storage_Azure_IoT, Storage_Dev_Locat, username);
+}
+void WriteDeviceLocation(std::string device_location)
+{
+    WriteFlash(Storage_WiFi_SSID, Storage_WiFi_Pass, Storage_Home_Name, Storage_Azure_IoT, device_location, Storage_User_Name);
 }
 
 bool HasValidWiFi()
@@ -129,6 +177,18 @@ bool HasValidAzure()
     return !Storage_Azure_IoT.empty();
 }
 
+bool HasUsername()
+{
+    ReadFlash();
+    return !Storage_User_Name.empty();
+}
+
+bool HasDeviceLocation()
+{
+    ReadFlash();
+    return !Storage_Dev_Locat.empty();
+}
+
 std::string GetFlashValue(EEPROM_VALUE value, bool readFlash)
 {
     if (readFlash)
@@ -143,6 +203,10 @@ std::string GetFlashValue(EEPROM_VALUE value, bool readFlash)
         return Storage_Home_Name;
     case EEPROM_VALUE::Azure_IoT_Hub:
         return Storage_Azure_IoT;
+    case EEPROM_VALUE::User_Name:
+        return Storage_User_Name;
+    case EEPROM_VALUE::Device_Location:
+        return Storage_Dev_Locat;
     default:
         return "";
     }
--- ESP32/lib/Storage/StoreHub.h
+++ ESP32/lib/Storage/StoreHub.h
@@ -6,6 +6,8 @@
 #define EEPROM_WIFI_PASSWORD_LEN_ADDR 0x01    // Address of the flash memory stores length of WiFi Password
 #define EEPROM_GOOGLE_HOME_NAME_LEN_ADDR 0x02 // Address of the flash memory stores length of Google Home Name
 #define EEPROM_AZURE_IOT_HUB_LEN_ADDR 0x03    // Address of the flash memory stores length of Google Home Name
+#define EEPROM_DEVICE_LOCATION_LEN_ADDR 0x04  // Address of the flash memory stores length of Google Home Name
+#define EEPROM_USER_NAME_LEN_ADDR 0x05        // Address of the flash memory stores length of Google Home Name
 #define EEPROM_STORAGE_START_ADDR 0x0A        // Starting address of the flash memory actual values are stored
 
 enum EEPROM_VALUE
@@ -13,17 +15,29 @@ enum EEPROM_VALUE
     WiFi_SSID,
     WiFi_Password,
     Google_Home_Name,
-    Azure_IoT_Hub
+    Azure_IoT_Hub,
+    Device_Location,
+    User_Name
 };
 
 void EraseFlash(bool safe = false);
 void ReadFlash();
-void WriteFlash(std::string wifi_ssid, std::string wifi_pass, std::string home_name, std::string iot_hub_connection_string);
+void WriteFlash(
+    std::string wifi_ssid,
+    std::string wifi_pass,
+    std::string home_name,
+    std::string iot_hub_connection_string,
+    std::string device_location,
+    std::string username);
+void WriteFlashUsername(std::string username);
+void WriteFlashDeviceLocation(std::string device_location);
 void WriteFlashHomeName(std::string home_name);
 void WriteAzureIoTHub(std::string connection_string);
 void WriteFlashWiFi(std::string wifi_ssid, std::string wifi_pass);
 bool HasValidWiFi();
 bool HasValidHome();
 bool HasValidAzure();
+bool HasUsername();
+bool HasDeviceLocation();
 
 std::string GetFlashValue(EEPROM_VALUE value, bool readFlash = false);
