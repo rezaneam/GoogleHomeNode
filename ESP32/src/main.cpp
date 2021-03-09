d2da717011b2b97385fa99d0679021d839ec9526
Reza <mr.naeemabadi@gmail.com>
2021-03-09 18:59:16 +0100

    ESP32 : Adding support for handling temperature summary

--- ESP32/src/main.cpp
+++ ESP32/src/main.cpp
@@ -26,7 +26,7 @@ void setup()
   pinMode(BLE_ADVERTISE_ENABLE_PIN, INPUT_PULLUP); // External PIN for triggering the advertise for BLE
   attachInterrupt(digitalPinToInterrupt(BLE_ADVERTISE_ENABLE_PIN), handleExternalInterrupt, FALLING);
 
-  wireless.Initialize(&EnqueueEvent, "GH-Home", VERBOSE);
+  wireless.Initialize(&EnqueueEvent, "GH-Home", (char *)VERBOSE);
 
   googleHome.Initialize(&EnqueueEvent, VERBOSE);
 
@@ -126,6 +126,13 @@ void loop()
     if (isHomeConnected)
       googleHome.NotifyTemperature((int)Sensor.Measurments.cur_temperature, azureIoT.GetLanguage());
     break;
+  case CustomEvents::EVENT_GOOGLE_REPORT_TEMPERATURE_SUMMARY:
+    if (isHomeConnected)
+      googleHome.NotifyTemperatureSummary(
+          (int)Sensor.Measurments.min_temperature,
+          (int)Sensor.Measurments.max_temperature,
+          azureIoT.GetLanguage());
+    break;
   case CustomEvents::EVENT_GOOGLE_REPORT_HUMIDITY:
     if (isHomeConnected)
       googleHome.NotifyHumidity((int)Sensor.Measurments.cur_humidity, azureIoT.GetLanguage());
@@ -184,8 +191,8 @@ void loop()
         float pressure = Sensor.Measurments.cur_pressure;
         float airQuality = Sensor.Measurments.cur_airQuality;
         if (VERBOSE)
-          printf(">> General Info %s Free Heap %d >> Temperature: %2.1fc(%2.1f-%2.1f) Humidity: %2.1f%%(%2.1f-%2.1f) Pressure: %2.2fatm AirQuality: %2.2f\r\n",
-                 String(ctime(&now)).c_str(), ESP.getFreeHeap(),
+          printf(">> General Info %s Free Heap %d%% >> Temperature: %2.1fc(%2.1f-%2.1f) Humidity: %2.1f%%(%2.1f-%2.1f) Pressure: %2.2fatm AirQuality: %2.2f\r\n",
+                 String(ctime(&now)).c_str(), 100 * ESP.getFreeHeap() / ESP.getHeapSize(),
                  temperature, Sensor.Measurments.min_temperature, Sensor.Measurments.max_temperature,
                  humidity, Sensor.Measurments.min_humidity, Sensor.Measurments.max_humidity,
                  pressure / 101325, airQuality);
@@ -195,8 +202,8 @@ void loop()
     }
     else
     {
-      printf(">> General Info %s Free Heap %d\r\n",
-             String(ctime(&now)).c_str(), ESP.getFreeHeap());
+      printf(">> General Info %s Free Heap %d%%\r\n",
+             String(ctime(&now)).c_str(), 100 * ESP.getFreeHeap() / ESP.getHeapSize());
     }
     readSenor = false;
   }
