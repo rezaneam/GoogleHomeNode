8d816fd63217374fba3ac4122bdd5bc6b8a38eeb
Reza <mr.naeemabadi@gmail.com>
2021-03-15 20:17:29 +0100

    ESP32 : moving the general configuration in the separate header file

---
+++ ESP32/src/config.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#define SENSOR_SDA_PIN 5
+#define SENSOR_SCL_PIN 4
+#define OLED_SDA_PIN 21
+#define OLED_SCL_PIN 22
+#define SDA_Frequency 400000
+#define OLED_Address 0x3c
+
+#define BLE_ADVERTISE_TIMEOUT_S 60  // BLE advertising timeout
+#define BLE_ADVERTISE_ENABLE_PIN 33 //02 // 12 //
+#define BLE_ADVERTISE_LED_PIN 12
+#define SENSOR_READ_INTERVAL 60
+
+#define EEPROM_SIZE 256
+#define SERIAL_BAUDRATE 115200
+
+#define VERBOSE true
--- ESP32/src/main.h
+++ ESP32/src/main.h
@@ -9,24 +9,9 @@
 #include <Azure.h>
 #include <GoogleHome.h>
 #include <BLElite.h>
+#include <config.h>
 
-#define SENSOR_SDA_PIN 5
-#define SENSOR_SCL_PIN 4
-#define OLED_SDA_PIN 21
-#define OLED_SCL_PIN 22
-#define SDA_Frequency 400000
-#define OLED_Address 0x3c
-
-#define BLE_ADVERTISE_TIMEOUT_S 60                              // BLE advertising timeout
 #define BLE_ADVERTISE_TIMEOUT_MS 1000 * BLE_ADVERTISE_TIMEOUT_S // BLE advertising timeout
-#define BLE_ADVERTISE_ENABLE_PIN 33                             //02 // 12 //
-#define BLE_ADVERTISE_LED_PIN 12
-#define SENSOR_READ_INTERVAL 60
-
-#define EEPROM_SIZE 256
-#define SERIAL_BAUDRATE 115200
-
-#define VERBOSE true
 
 EnvironmentSensor Sensor;
 SSD1306Wire Oled(OLED_Address, OLED_SDA_PIN, OLED_SCL_PIN);
